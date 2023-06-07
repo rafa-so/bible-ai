95098bb87dcbbb4b339500b736e1c298a5157ccc
Rafael Oliveira <rafael.oliveira@culturainglesa.com.br>
2023-06-07 16:31:01 -0300

    wip: create flow to search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,5 @@
 class SearchController < ApplicationController
   def index
+    @response = "Teste de pesquisa"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'search/index'
 
-  root 'search#index'
+  root 'welcome#index'
 end
