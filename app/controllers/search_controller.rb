689c8a5e48c623132a082402208edec3e8383809
Rafael Oliveira <rafael.oliveira@culturainglesa.com.br>
2023-06-09 13:47:47 -0300

    fix: adjust text template and add gem debugging

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,12 +2,15 @@ class SearchController < ApplicationController
   before_action :set_client
 
   def index
-    @response = ''
+    search_param = params['query']
+
+    query_string = "Retorne todos os versículos com o termo: #{search_param}.
+      Os versículos deverão estar separados por tags html de lista desordetada"
 
     gpt_response = @client.chat(
       parameters: {
         model: 'gpt-3.5-turbo-0301',
-        messages: [{ role: 'user', content: 'Me diga olá!' }]
+        messages: [{ role: 'user', content: query_string }]
       }
     )
 
