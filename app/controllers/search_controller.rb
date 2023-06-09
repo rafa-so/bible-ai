36168e5325dc86933d95e85d10e333ee62aef865
Rafael Oliveira <rafael.oliveira@culturainglesa.com.br>
2023-06-09 16:01:55 -0300

    fix elements within list page and created search toggle controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,12 +7,22 @@ class SearchController < ApplicationController
     query_string = "Retorne todos os versículos com o termo: #{search_param}.
       Os versículos deverão estar separados por tags html de lista desordetada"
 
-    gpt_response = @client.chat(
-      parameters: {
-        model: 'gpt-3.5-turbo-0301',
-        messages: [{ role: 'user', content: query_string }]
-      }
-    ).dig("choices", 0, "message", "content")
+    if ENV['OPENAI_ACCESS_TOKEN'].present?
+      gpt_response = @client.chat(
+        parameters: {
+          model: 'gpt-3.5-turbo-0301',
+          messages: [{ role: 'user', content: query_string }]
+        }
+      ).dig("choices", 0, "message", "content")
+    else
+      gpt_response = "
+        <ul>
+          <li>Jonas 1:2 - \"Levanta-te, vai à grande cidade de Nínive e clama contra ela, porque a sua malícia subiu até à minha presença.\"</li>
+          <li>Jonas 3:3 - \"E levantou-se Jonas, e foi a Nínive, segundo a palavra do SENHOR. Ora, Nínive era uma cidade muito grande, de três dias de caminho.\"</li>
+          <li>Jonas 3:4 - \"E começou Jonas a entrar pela cidade caminho de um dia, e pregava, dizendo: Ainda quarenta dias, e Nínive será subvertida.\"</li>
+        </ul>
+      "
+    end
 
     @response = build_array_response gpt_response
   end
