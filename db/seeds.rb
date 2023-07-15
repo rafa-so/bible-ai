5a480bbdf918b673b803d206616c800f34de8451
Rafael <rafa.forsoft.25@gmail.com>
2023-07-15 19:48:45 -0300

    fix: criação de usuário padrão

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-User.create(email: "admin@email.com", password: "123123")
+User.create(email: ENV['DEFAULT_USER_EMAIL'], password: ENV['DEFAULT_USER_PASSWORD']) unless User.where(email: ENV['DEFAULT_USER_EMAIL'])
